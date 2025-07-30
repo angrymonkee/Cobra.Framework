a2220c0ad17572cd5d724f57d57587b51e7eaeb9
dajon <dajon@microsoft.com>
2025-07-30 16:40:19 -0400

    Added mock data to sysconfig

--- sysconfig.ps1
+++ sysconfig.ps1
@@ -1,11 +1,11 @@
 # Global settings for the application
 $global:CobraConfig = @{
-    OwnerEmail             = "<your email address>"
-    CodeRoot               = $env:CODE_REPO
-    CobraRoot              = $env:COBRA_ROOT
+    OwnerEmail             = "<your email address>" # test@test.com
+    CodeRepo               = # C:\Code
+    CobraRoot              = # C:\Code\Cobra.Framework
 
     # This can be any location. Ideally should be a shared location so 
     # multiple users can benefit by sharing modules. Import will automatically 
     # look here if no artifact location is specified.
-    ModuleRegistryLocation = "$env:COBRA_ROOT\CobraModuleRegistry"
+    ModuleRegistryLocation = "C:\Code\CobraModuleRegistry"
 }
